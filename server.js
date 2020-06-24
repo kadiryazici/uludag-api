668231fa78a4c6cafa396ac69a3f2bfa0d8f4be3
kadiryazici <kyzc411@outlook.com.tr>
2020-06-24 17:18:20 +0300

    Changed Cors mode again

--- server.js
+++ server.js
@@ -5,6 +5,17 @@ const fetchData = require("./getData");
 
 const port = process.env.PORT || 3000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 app.get("/", (_, response) => {
   fetchData((entryObject) => {
     response.send(JSON.stringify(entryObject));
